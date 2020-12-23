a218cb947eb5cfd4338bc4d722df9d189e77767a
Tomas Durkin <tdurkin1@gmail.com>
2020-12-22 17:54:37 -0800

    added light control and bubble particles

--- SubCommander/Assets/Scripts/submarineController.cs
+++ SubCommander/Assets/Scripts/submarineController.cs
@@ -8,7 +8,8 @@ public class submarineController : MonoBehaviour
     [SerializeField] float subSpeed = 0.2f;
     [SerializeField] float moveStep = 1f;
     [SerializeField] float rotateStep = 1f;
-    [SerializeField] GameObject sub;
+    [SerializeField] Light subLight;
+    [SerializeField] ParticleSystem propBubbles;
     
     private bool periscope;
     public bool Periscope
@@ -16,15 +17,19 @@ public class submarineController : MonoBehaviour
         get { return periscope; }
         set { periscope = value; }
     }
-    
+
+    private bool lightOn;
+
     void Start()
     {
         periscope = false;
+        lightOn = false;
     }
 
     void Update()
     {
         HandleMovement();
+        HandleLight();
     }
 
     private void HandleMovement()
@@ -35,31 +40,61 @@ public class submarineController : MonoBehaviour
             if (Input.GetKey("q"))
             {
                 Debug.Log("up");
-                Vector3 targetPos = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+                Vector3 targetPos = new Vector3(transform.position.x, transform.position.y + moveStep, transform.position.z);
                 transform.position = Vector3.Lerp(transform.position, targetPos, subSpeed * Time.deltaTime);
+                propBubbles.Play();
             }
             else if (Input.GetKey("e"))
             {
                 Debug.Log("down");
-                Vector3 targetPos = new Vector3(transform.position.x, transform.position.y - 1, transform.position.z);
+                Vector3 targetPos = new Vector3(transform.position.x, transform.position.y - moveStep, transform.position.z);
                 transform.position = Vector3.Lerp(transform.position, targetPos, subSpeed * Time.deltaTime);
+                propBubbles.Play();
             }
             else if (Input.GetKey("w"))
             {
                 Debug.Log("forward");
-                Vector3 targetPos = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
+                Vector3 targetPos = new Vector3(transform.position.x - moveStep, transform.position.y, transform.position.z);
                 transform.position = Vector3.Lerp(transform.position, targetPos, subSpeed * Time.deltaTime);
+                propBubbles.Play();
             }
             else if (Input.GetKey("s"))
             {
                 Debug.Log("back");
-                Vector3 targetPos = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
+                Vector3 targetPos = new Vector3(transform.position.x + moveStep, transform.position.y, transform.position.z);
                 transform.position = Vector3.Lerp(transform.position, targetPos, subSpeed * Time.deltaTime);
+                propBubbles.Play();
+            }
+            else
+            {               
+                StartCoroutine(stopBubbles());
             }
         }
         else
         {
             //control periscope
+        }        
+    }
+    void HandleLight()
+    {
+        if (Input.GetKeyDown("l"))
+        {
+            if (lightOn)
+            {
+                subLight.gameObject.SetActive(false);
+                lightOn = false;
+            }
+            else
+            {
+                subLight.gameObject.SetActive(true);
+                lightOn = true;
+            }
         }
+    }
+
+    IEnumerator stopBubbles()
+    {
+        yield return new WaitForSeconds(0.1f);
+        propBubbles.Stop();
     }
 }
